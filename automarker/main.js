e461dc664ded768095ffd59fd9646575ebffefe9
7thAce <7thAce@users.noreply.github.com>
2026-05-28 01:03:38 -0600

    Add autostart code
    more prints

--- automarker/main.js
+++ automarker/main.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
 					kevangobtn.disabled = false;
 					kevungobtn.disabled = false;
 					kevongobtn.disabled = false;
-				};
+				}
 				if (jsonMessage.type == "game_start") {
 					let restart = jsonMessage.data.restart ?? "false";
 					if (restart == "true") {
@@ -316,7 +316,10 @@ document.addEventListener("DOMContentLoaded", () => {
 function sendGarageWarp() {
 	const WARP_CODE = {"cat":0,"sub":7,"b":0,"dw1":9,"dw2":900,"dw3":0};
 	try {
+		console.log("Attempting to send garage warp...");
+		conn1.send("Yippee");
 		conn1.send(JSON.stringify(WARP_CODE));
+		console.log("Send complete");
 		// conn2.send(JSON.stringify(WARP_CODE));
 	} catch(error) {
 		console.error("Failed to send:", error);
